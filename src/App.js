27117107efa690a5a1bc907b865d0fcd47e9d4dd
Gibrahn Duarte <gibrahn.duarte18@tectijuana.edu.mx>
2022-07-26 18:01:21 -0700

    Remove Routes

--- src/App.js
+++ src/App.js
@@ -2,16 +2,10 @@ import "./App.css";
 import NavBar from "./Component/NavBar";
 import Catalog from "./Component/Catalog";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 function App() {
   return (
     <>
       <NavBar />
-
-      <Routes>
-        <Route path="/Catalog" element={<Catalog />}></Route>
-      </Routes>
     </>
   );
 }
